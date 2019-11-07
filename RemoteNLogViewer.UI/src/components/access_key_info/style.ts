9c5ef0374676e23977aeb2a37af21e2b30661d6d
Eliezer Souza <eliezervinicius@outlook.com>
2019-11-06 23:21:24 -0300

    Some adjustments

--- RemoteNLogViewer.UI/src/components/access_key_info/style.ts
+++ RemoteNLogViewer.UI/src/components/access_key_info/style.ts
@@ -1,7 +1,5 @@
 import styled from "../../theme/styled";
 
-const AccessKeyInfoWrapper = styled.div`
-  /* width: 100%; */
-`;
+const AccessKeyInfoWrapper = styled.div``;
 
 export default AccessKeyInfoWrapper;
--- RemoteNLogViewer.UI/src/components/connection_info/style.ts
+++ RemoteNLogViewer.UI/src/components/connection_info/style.ts
@@ -2,7 +2,6 @@ import styled from "../../theme/styled";
 
 const ConnectionInfoWrapper = styled.div`
   margin-bottom: 10px;
-  /* width: 100%; */
 `;
 
 export default ConnectionInfoWrapper;
